2dc11efa6a9b98936ea3b84f416ed06e8c986354
Zahid <zahid@zmdev.com>
2023-08-06 18:06:07 +0100

    fixed readme link

--- components/home-section/extra-project-item.jsx
+++ components/home-section/extra-project-item.jsx
@@ -5,7 +5,7 @@ export default function ExtraProjectItem(props) {
     const { name, tags = [], description, thumburl, id } = project;
 
     return (
-        <Link href={`/projects/${id}/readme`}>
+        <Link href={`/projects/${id}`}>
             <article className="w-full relative flex flex-col space-y-2">
                 <figure className="w-full aspect-square rounded-lg overflow-hidden bg-gray-100">
                     <img src={thumburl} className="w-full object-cover object-center h-full" />
