b8efbdbb8cdf215c790bdb1a3a677b7bb6fe8b98
Zahid <zahid@zmdev.com>
2023-09-02 18:53:39 +0100

    undo comments

--- services/fetch.js
+++ services/fetch.js
@@ -2,7 +2,6 @@ import contents from "@/data/contents";
 import repo from "@/data/repo";
 
 export async function fetchProjects() {
-    return [];
     const res = await fetch(`http://localhost:3000/api/projects`);
     const json = await res.json();
     return json.projects;
@@ -11,20 +10,17 @@ export async function fetchProjects() {
 export async function fetchGithubRepo(id) {
     // const res = await fetch(`https://api.github.com/repos/${name}`);
     // const json = await res.json();
-    return [];
     const json = repo;
     return json;
 }
 
 export async function fetchProject(id) {
-    return {};
 	const res = await fetch(`http://localhost:3000/api/projects/${id}`);
 	const json = await res.json();
 	return json.project;
 }
 
 export async function fetchContents(name) {
-    return {};
     // const res = await fetch(`https://api.github.com/repos/${name}/contents`);
     // const json = await res.json();
     const json = contents;
@@ -32,7 +28,6 @@ export async function fetchContents(name) {
 }
 
 export async function fetchSections() {
-    return [];
     const res = await fetch(`http://localhost:3000/api/categories`, { cache: 'no-store' });
     const json = await res.json();
     return json.categories;
