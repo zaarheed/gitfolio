11a11211e744987ab65a86ab94ecf0fa0e4e7515
Zahid <zahid@zmdev.com>
2023-09-02 18:53:25 +0100

    force working build

--- services/fetch.js
+++ services/fetch.js
@@ -2,6 +2,7 @@ import contents from "@/data/contents";
 import repo from "@/data/repo";
 
 export async function fetchProjects() {
+    return [];
     const res = await fetch(`http://localhost:3000/api/projects`);
     const json = await res.json();
     return json.projects;
@@ -10,17 +11,20 @@ export async function fetchProjects() {
 export async function fetchGithubRepo(id) {
     // const res = await fetch(`https://api.github.com/repos/${name}`);
     // const json = await res.json();
+    return [];
     const json = repo;
     return json;
 }
 
 export async function fetchProject(id) {
+    return {};
 	const res = await fetch(`http://localhost:3000/api/projects/${id}`);
 	const json = await res.json();
 	return json.project;
 }
 
 export async function fetchContents(name) {
+    return {};
     // const res = await fetch(`https://api.github.com/repos/${name}/contents`);
     // const json = await res.json();
     const json = contents;
@@ -28,6 +32,7 @@ export async function fetchContents(name) {
 }
 
 export async function fetchSections() {
+    return [];
     const res = await fetch(`http://localhost:3000/api/categories`, { cache: 'no-store' });
     const json = await res.json();
     return json.categories;
